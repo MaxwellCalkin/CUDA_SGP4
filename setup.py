c0ef602f9249dd9d099a08c2b043323d80a652b5
MaxwellCalkin <mcalkin@intaptai.com>
2025-05-27 13:06:40 -0400

    deleted unnecessary files and filled out setup.py dependencies

--- setup.py
+++ setup.py
@@ -5,18 +5,21 @@ setup(
     version='0.1.0',
     packages=find_packages(),
     install_requires=[
-        'numpy',
-        'numba',
-        # Add other dependencies here
+        'numpy>=1.24.0',
+        'numba>=0.59.1',
+        'astropy>=5.2.1',
     ],
-    author='Your Name',
-    author_email='your.email@example.com',
+    tests_require=[
+        'pytest>=8.3.3',
+    ],
+    author='Max Calkin',
+    author_email='mcalkin@intaptai.com',
     description='CUDA-accelerated SGP4 orbit propagation package',
-    url='https://github.com/yourusername/cuda_sgp4',  # Replace with your repository URL
+    url='https://github.com/maxwellcalkin/cuda_sgp4',  # Replace with your repository URL
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.10',
 )
